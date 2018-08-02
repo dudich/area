d9b4ccf7146e11bb25ebd8cba801005d6e254e13
dudich <dudich.dev@gmail.com>
2018-08-02 18:16:11 +0300

    add EventDetails component

---
+++ src/components/EventList/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-carousel>
+    <v-carousel-item :key="i" v-for="i in Math.ceil(items.length / 4)">
+      <v-layout row>
+        <v-flex xs3 :key="j" v-for="(item, j) in items">
+          <img :src="item.src" alt="">
+        </v-flex>
+      </v-layout>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'icons/event-types/event-type-icon-1.png'
+          },
+          {
+            src: 'icons/event-types/event-type-icon-1.png'
+          },
+          {
+            src: 'icons/event-types/event-type-icon-1.png'
+          },
+          {
+            src: 'icons/event-types/event-type-icon-1.png'
+          },
+          {
+            src: 'icons/event-types/event-type-icon-1.png'
+          }
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <div class="event-details" :style="{ backgroundImage: `url(${bgImage})` }">
+      <h2 class="event-details__caption" v-html="caption"></h2>
+      <stage-list></stage-list>
+      <event-list></event-list>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  import StageList from '../components/StageList'
+  import EventList from '../components/EventList'
+  import bgImage from '../assets/event-details-bg.png'
+
+  export default {
+    name: 'event-details',
+
+    data() {
+      return {
+        bgImage,
+        caption: 'Tailored Solutions for Your Event'
+      }
+    },
+
+    components: {
+      StageList,
+      EventList
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .event-details {
+    padding: 20px;
+    background: {
+      repeat: no-repeat;
+      size: cover;
+    }
+
+    &__caption {
+      margin: {
+        bottom: 30px;
+      }
+      font-size: 40px;
+      line-height: 55px;
+      font-weight: 700;
+      color: white;
+    }
+  }
+</style>
