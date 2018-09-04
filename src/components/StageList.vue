24a39c17025cdf1d48a62b2385bd038eac4631ca
dudich <dudich.dev@gmail.com>
2018-09-04 18:22:39 +0300

    refactor stageList, add stage-number

--- src/components/StageList.vue
+++ src/components/StageList.vue
@@ -13,26 +13,42 @@
           </v-btn>
         </v-flex>
         <v-flex :class="[pathCondition ? 'xs12 sm4 md3 lg3' : 'xs12 sm4']">
-          <h3 class="booking-stage" v-text="bookingStages.selectEvent"
-              :class="{ active: $route.path === '/select-event'}"></h3>
+          <h3 class="booking-stage"
+              v-text="bookingStages.selectEvent"
+              :class="{ active: $route.path === '/select-event'}"
+          >
+          </h3>
+          <span class="booking-stage-number hidden-sm-and-up">1/3</span>
         </v-flex>
         <v-flex :class="[pathCondition ? 'xs12 sm4 md3 lg4' : 'xs12 sm4']">
-          <h3 class="booking-stage" v-text="bookingStages.selectPackage"
-              :class="{ active: $route.path === '/select-package'}"></h3>
+          <h3 class="booking-stage"
+              v-text="bookingStages.selectPackage"
+              :class="{ active: $route.path === '/select-package'}"
+          >
+          </h3>
+          <span class="booking-stage-number hidden-sm-and-up">2/3</span>
         </v-flex>
         <v-flex
           :class="[pathCondition ? 'xs12 sm4 lg4' : 'xs12 sm4']"
           v-if="$route.path === `/hold/${ $route.params.id }`"
         >
-          <h3 class="booking-stage" v-text="bookingStages.hold"
-              :class="{ active: $route.path === `/hold/${ $route.params.id }`}"></h3>
+          <h3 class="booking-stage"
+              v-text="bookingStages.hold"
+              :class="{ active: $route.path === `/hold/${ $route.params.id }`}"
+          >
+          </h3>
+          <span class="booking-stage-number hidden-sm-and-up">3/3</span>
         </v-flex>
         <v-flex
           :class="[pathCondition ? 'xs12 sm4 lg4' : 'xs12 sm4']"
           v-else
         >
-          <h3 class="booking-stage" v-text="bookingStages.book"
-              :class="{ active: $route.path === `/book/${ $route.params.id }`}"></h3>
+          <h3 class="booking-stage"
+              v-text="bookingStages.book"
+              :class="{ active: $route.path === `/book/${ $route.params.id }`}"
+          >
+          </h3>
+          <span class="booking-stage-number hidden-sm-and-up">3/3</span>
         </v-flex>
       </v-layout>
     </v-container>
@@ -73,9 +89,22 @@
       font-weight: 400;
       color: white;
 
+      &-number {
+        display: none;
+        margin-left: 10px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #fff;
+      }
+
       &.active {
+        display: inline-block;;
         font-weight: bold;
         color: $blue;
+
+        & + .booking-stage-number {
+          display: inline-block;
+        }
       }
 
       @media screen and (min-width: $xs) {
@@ -84,16 +113,16 @@
           bottom: 16px;
         }
       }
+
+      @media screen and (max-width: $xs - 1) {
+        display: none;
+      }
     }
 
     .back-link {
       padding: 20px 32px;
       border: 1px solid $blue;
       font-size: 18px;
-    }
-
-    @media screen and (max-width: $xs - 1) {
-      padding-bottom: 10px;
     }
   }
 </style>
